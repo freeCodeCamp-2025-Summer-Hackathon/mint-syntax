eae9166d4f57fc5af63e9cfc8aba9d4b82e75159
willhitman <giftwt9wt@gmail.com>
2025-07-07 13:31:34 +0200

    adding csrf routes to api

--- backend/src/api/main.py
+++ backend/src/api/main.py
@@ -1,7 +1,9 @@
 from fastapi import APIRouter
 
+from src.api.csrf_security import csrf_routes
 from src.api.routes import ideas, users
 
 api_router = APIRouter()
 api_router.include_router(ideas.router)
 api_router.include_router(users.router)
+api_router.include_router(csrf_routes.router, tags=["CSRF"])
