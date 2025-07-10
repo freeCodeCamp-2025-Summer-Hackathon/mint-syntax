1daaad2e6e7df382062fc77ed36893dfbcc6fcae
Sebastian_Wlo <sebastianwlodarczyk92@gmail.com>
2025-07-10 17:33:31 +0200

    feat: add a route to a "register new user" page
    Adds a new route to the "main.jsx" file, and creates a "Register.jsx" file for displaying the page.

---
+++ frontend/src/Register.jsx
@@ -0,0 +1,16 @@
+import Header from './components/Header.jsx';
+import Footer from './components/Footer.jsx';
+import './styles.css';
+
+function Register() {
+  return (
+    <div className='body-style'>
+       <div className='container-wrapper'>
+        <Header />
+        <Footer />
+       </div>
+    </div>
+  );
+}
+
+export default Register;
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,15 +1,20 @@
 import { StrictMode } from 'react';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { createRoot } from 'react-dom/client';
+import { HelmetProvider } from 'react-helmet-async';
 import './index.css';
 import App from './App.jsx';
-import { HelmetProvider } from 'react-helmet-async';
+import Register from './Register.jsx';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <App />,
   },
+  {
+    path: '/register',
+    element: <Register />,
+  }
 ]);
 
 createRoot(document.getElementById('root')).render(
