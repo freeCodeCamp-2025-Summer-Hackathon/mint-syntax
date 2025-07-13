7648d5ca5669d13da5192268ac8e7c8c4a710b2a
willhitman <giftwt9wt@gmail.com>
2025-07-13 10:28:56 +0200

    extracted feature card and team card from LandingPageComponent.jsx and placed in this folder

---
+++ frontend/src/components/landingPageComponents/TeamGrid.jsx
@@ -0,0 +1,118 @@
+import React from 'react';
+import '../../styles.css';
+
+const TeamGrid = () => {
+  return (
+    <>
+      <h3 id='about-team-section' className='team-heading'>
+        Team
+      </h3>
+      <div className='team-grid'>
+        <span className='team-member'>Apofus</span>
+        <span className='team-member'>
+          <a
+            href='https://github.com/bstojkovic'
+            className='team-link'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            Bożo (Coding Puppy)
+          </a>
+        </span>
+        <span className='team-member'>
+          <a
+            href='https://github.com/connororeil'
+            className='team-link'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            Connor
+          </a>
+        </span>
+        <span className='team-member'>
+          <a
+            href='https://github.com/willhitman'
+            className='team-link'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            Gift Ruwende
+          </a>
+        </span>
+        <span className='team-member'>
+          <a
+            href='https://github.com/MarkoCuk54'
+            className='team-link'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            longlive247
+          </a>
+        </span>
+        <span className='team-member'>
+          <a
+            href='https://github.com/Lorevdh'
+            className='team-link'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            Lore
+          </a>
+        </span>
+        <span className='team-member'>Millicent</span>
+        <span className='team-member'>
+          <a
+            href='https://github.com/Vallayah'
+            className='team-link'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            Ola
+          </a>
+        </span>
+        <span className='team-member'>
+          <a
+            href='https://github.com/gikf'
+            className='team-link'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            Krzysztof
+          </a>
+        </span>
+        <span className='team-member'>
+          <a
+            href='https://github.com/Sebastian-Wlo'
+            className='team-link'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            Sebastian_W
+          </a>
+        </span>
+        <span className='team-member'>
+          <a
+            href='https://github.com/tetrisy'
+            className='team-link'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            Tetris
+          </a>
+        </span>
+        <span className='team-member'>
+          <a
+            href='https://github.com/nurmukhammad03'
+            className='team-link'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            VooDooRe
+          </a>
+        </span>
+      </div>
+    </>
+  );
+};
+
+export default TeamGrid;
