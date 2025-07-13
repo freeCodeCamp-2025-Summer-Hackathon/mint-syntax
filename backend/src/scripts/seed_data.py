85decb27ba0d41d6bf0f615aed4128eb263e9411
Connor <71609195+connororeil@users.noreply.github.com>
2025-07-13 14:44:42 +0200

    fix: add votes to user data
    also fix to match new database and auth methods

--- backend/src/scripts/seed_data.py
+++ backend/src/scripts/seed_data.py
@@ -3,18 +3,26 @@ import asyncio
 from faker import Faker
 from odmantic import ObjectId
 
-from src.database import engine
+from src.auth import get_password_hash
+from src.database import get_engine
 from src.models import Idea, User
 
 fake = Faker()
 
 
 def generate_user() -> User:
-    return User.model_validate({"name": fake.name()})
+    return User.model_validate(
+        {
+            "name": fake.name(),
+            "username": fake.user_name(),
+            "hashed_password": get_password_hash(fake.password()),
+        }
+    )
 
 
 async def seed_users(num_users: int = 10) -> list[User]:
     new_users = [generate_user() for _ in range(num_users)]
+    engine = await get_engine()
     await engine.save_all(new_users)
     return new_users
 
@@ -23,7 +31,7 @@ def generate_votes(voter_ids: list[ObjectId]) -> list[ObjectId]:
     return list(fake.random_elements(voter_ids, unique=True))
 
 
-def generate_idea(user_ids: list[ObjectId]) -> Idea:
+def generate_idea(user_ids: list[ObjectId], user_lookup: dict[ObjectId, User]) -> Idea:
     creator_id = fake.random_element(user_ids)
     new_idea = Idea.model_validate(
         {
@@ -32,22 +40,34 @@ def generate_idea(user_ids: list[ObjectId]) -> Idea:
             "creator_id": creator_id,
         }
     )
+
     for voter_id in generate_votes(user_ids):
         if voter_id != creator_id:
-            fake.random_element([new_idea.downvoted_by, new_idea.upvoted_by]).append(
-                voter_id
-            )
+            up_vote_bool = fake.random_element([True, False])
+            voter = user_lookup[voter_id]
+            if up_vote_bool:
+                new_idea.upvoted_by.append(voter_id)
+                voter.upvotes.append(new_idea.id)
+            else:  # downvote
+                new_idea.downvoted_by.append(voter_id)
+                voter.downvotes.append(new_idea.id)
     return new_idea
 
 
 async def seed_ideas(user_ids: list[ObjectId], num_ideas: int = 20) -> list[Idea]:
-    new_ideas = [generate_idea(user_ids) for _ in range(num_ideas)]
+    engine = await get_engine()
+    users = await engine.find(User)
+    user_lookup = {user.id: user for user in users}
+    new_ideas = [generate_idea(user_ids, user_lookup) for _ in range(num_ideas)]
+
     await engine.save_all(new_ideas)
     return new_ideas
 
 
+# TODO: add optional database pruning before seeding
 async def main():
     await seed_users()
+    engine = await get_engine()
     users = await engine.find(User)
     user_ids = [user.id for user in users]
     if not user_ids:
