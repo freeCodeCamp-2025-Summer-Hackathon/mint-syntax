709952e462471aa181768595564a5c576ca568da
Sebastian_Wlo <sebastianwlodarczyk92@gmail.com>
2025-07-14 20:07:43 +0200

    chore: move RegisterForm.jsx to the "/components" directory

---
+++ frontend/src/components/RegisterForm.jsx
@@ -0,0 +1,143 @@
+import { useRef } from 'react';
+import { useForm } from 'react-hook-form';
+
+export function RegisterForm() {
+  const formRef = useRef();
+  const {
+    formState: { errors, isSubmitting, isValid },
+    handleSubmit,
+    register,
+  } = useForm();
+
+  const onSubmit = e => {
+    console.log(e);
+    if (e.password !== e.password2) console.log("passwords don't match");
+  };
+
+  return (
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      className='flex flex-col items-center'
+    >
+      <label className='floating-label'>
+        Username:
+        <label class='input input-sm'>
+          <svg
+            class='h-[1em] opacity-50'
+            xmlns='http://www.w3.org/2000/svg'
+            viewBox='0 0 24 24'
+          >
+            <g
+              stroke-linejoin='round'
+              stroke-linecap='round'
+              stroke-width='2.5'
+              fill='none'
+              stroke='currentColor'
+            >
+              <path d='M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2'></path>
+              <circle cx='12' cy='7' r='4'></circle>
+            </g>
+          </svg>
+
+          <input
+            {...register('userName')}
+            type='Text'
+            placeholder='Username'
+            className='input-validator'
+            required
+            //pattern='[A-Za-z][A-Za-z0-9\-]*'
+            //title='Only letters, numbers or dash'
+            defaultValue={'bob'}
+          />
+        </label>
+      </label>
+
+      <label className='floating-label'>
+        Name:
+        <label class='input input-sm'>
+          <input
+            {...register('name')}
+            type='Text'
+            placeholder='Name'
+            className='input-validator'
+            required
+            //pattern='[A-Za-z][A-Za-z0-9\-]*'
+            //title='Only letters, numbers or dash'
+            defaultValue={'bob'}
+          />
+        </label>
+      </label>
+
+      <label className='floating-label'>
+        Password:
+        <label class='input input-sm'>
+          <svg
+            class='h-[1em] opacity-50'
+            xmlns='http://www.w3.org/2000/svg'
+            viewBox='0 0 24 24'
+          >
+            <g
+              stroke-linejoin='round'
+              stroke-linecap='round'
+              stroke-width='2.5'
+              fill='none'
+              stroke='currentColor'
+            >
+              <path d='M2.586 17.414A2 2 0 0 0 2 18.828V21a1 1 0 0 0 1 1h3a1 1 0 0 0 1-1v-1a1 1 0 0 1 1-1h1a1 1 0 0 0 1-1v-1a1 1 0 0 1 1-1h.172a2 2 0 0 0 1.414-.586l.814-.814a6.5 6.5 0 1 0-4-4z'></path>
+              <circle cx='16.5' cy='7.5' r='.5' fill='currentColor'></circle>
+            </g>
+          </svg>
+
+          <input
+            {...register('password')}
+            type='Password'
+            placeholder='Password'
+            //required
+            /*pattern='(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}'*/
+            //title='Must be more than 8 characters, including number, lowercase letter, uppercase letter'
+            defaultValue={'123'}
+          />
+        </label>
+      </label>
+      {/*
+          <label className='form-label'>
+            Repeat Password:
+          </label>
+
+          <label class='input input-sm'>
+            <svg
+              class='h-[1em] opacity-50'
+              xmlns='http://www.w3.org/2000/svg'
+              viewBox='0 0 24 24'
+            >
+              <g
+                stroke-linejoin='round'
+                stroke-linecap='round'
+                stroke-width='2.5'
+                fill='none'
+                stroke='currentColor'
+              >
+                <path d='M2.586 17.414A2 2 0 0 0 2 18.828V21a1 1 0 0 0 1 1h3a1 1 0 0 0 1-1v-1a1 1 0 0 1 1-1h1a1 1 0 0 0 1-1v-1a1 1 0 0 1 1-1h.172a2 2 0 0 0 1.414-.586l.814-.814a6.5 6.5 0 1 0-4-4z'></path>
+                <circle cx='16.5' cy='7.5' r='.5' fill='currentColor'></circle>
+              </g>
+            </svg>
+            <input
+              name='passwordRepeat'
+              //{...register('password2', { required: true })}
+              type='password'
+              //required
+              placeholder='Password'
+              //pattern='(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}'
+              title='Must match the password entered in the previous input field'
+            />
+          </label>
+          */}
+
+      <div>
+        <button className='my-1 animated-button'>Register</button>
+      </div>
+    </form>
+  );
+}
+
+export default RegisterForm;
