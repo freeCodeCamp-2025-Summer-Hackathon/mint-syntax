c82309a9dc41e829c355e5cebd4d21d2db86eebf
willhitman <giftwt9wt@gmail.com>
2025-07-17 12:16:37 +0200

    ... refactoring, objects with no github uri dont render names as a link

--- frontend/src/components/TeamMemberCard.jsx
+++ frontend/src/components/TeamMemberCard.jsx
@@ -2,18 +2,20 @@ const TeamMemberCard = ({ github_uri, name }) => {
   // Team members data
 
   return (
-    <>
-      <span className='team-member'>
+    <span className='team-member'>
+      {github_uri ? (
         <a
-          href={`${guthub_uri || ''}`}
+          href={github_uri}
           className='team-link'
           target='_blank'
           rel='noopener noreferrer'
         >
           {name}
         </a>
-      </span>
-    </>
+      ) : (
+        name
+      )}
+    </span>
   );
 };
 
