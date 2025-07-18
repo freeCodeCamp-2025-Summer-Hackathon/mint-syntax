9b2b30f9c6b5515391c1ff1cd1132e0877ecbe0b
Sebastian_Wlo <sebastianwlodarczyk92@gmail.com>
2025-07-18 22:46:44 +0200

    style: add 'section-heading' class to the RegisterPage header

--- frontend/src/pages/RegisterPage.jsx
+++ frontend/src/pages/RegisterPage.jsx
@@ -4,7 +4,7 @@ function RegisterPage() {
   return (
     <div className='container-wrapper'>
       <section className='section-card flex flex-col justify-center items-center'>
-        <h3>Register as a new user:</h3>
+        <h3 className='section-heading'>Register as a new user:</h3>
         <div className='card bg-base-100 w-lg p-4 auto'>
           <RegisterForm />
         </div>
