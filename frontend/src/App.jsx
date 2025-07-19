f52138b3955a17db679f1bf210268e68711b4fd7
Aleksandra Filipczak <206249386+Vallayah@users.noreply.github.com>
2025-07-19 21:29:45 +0200

    Revert "Merge branch 'main' into me/pages"
    This reverts commit af137b11c5c75a271b9450191454dbddcb0dc4dc, reversing
    changes made to 01beb259c1b2e3e22de15c98ecdb01ae525ae4b6.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,10 +8,9 @@ import HelpPage from './components/HelpPage';
 import LoginPage from './pages/LoginPage';
 import ForgotPassword from './pages/ForgotPassword';
 import RegisterPage from './pages/RegisterPage';
-import MePage from './pages/MePage'; // From me/pages branch
-import MeEditPage from './pages/MeEditPage'; // From me/pages branch
-import MyIdeasPage from './pages/MyIdeasPage'; // From me/pages branch
-import SearchPage from './pages/SearchPage'; // From main branch
+import MePage from './pages/MePage';
+import MeEditPage from './pages/MeEditPage';
+import MyIdeasPage from './pages/MyIdeasPage';
 
 import { IdeaAddPage, IdeaEditPage, IdeaPage, IdeasPage } from './pages/Ideas';
 import './styles.css';
@@ -43,7 +42,6 @@ function App() {
           <Routes>
             <Route path='' element={<LandingPage />} />
             <Route path='help' element={<HelpPage />} />
-            <Route path='search' element={<SearchPage />} />
             <Route path='login' element={<LoginPage />} />
             <Route path='forgot-password' element={<ForgotPassword />} />
             <Route path='register' element={<RegisterPage />} />
