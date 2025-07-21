3891d9fb62546e1dd6d5ef44b746c1e472481e07
Aleksandra Filipczak <206249386+Vallayah@users.noreply.github.com>
2025-07-21 22:01:54 +0200

    Update VoteButtons.jsx

--- frontend/src/components/VoteButtons.jsx
+++ frontend/src/components/VoteButtons.jsx
@@ -79,44 +79,36 @@ const Button = ({
   );
 };
 
-export const DownvoteButton = ({ ideaId, onSuccess, onError }) => {
-  const { userState } = useUser();
-  return (
-    <Button
-      {...{
-        buttonProps: {
-          className: 'image-only-downvote-button',
-          ...(userState.downvotes.has(ideaId) && { disabled: true }),
-        },
-        buttonContents: (
-          <img src={DownvoteButtonImg} alt='Downvote' className='downvote-icon' />
-        ),
-        fetchAddr: `/ideas/${ideaId}/downvote`,
-        ideaId,
-        onSuccess,
-        onError,
-      }}
-    />
-  );
-};
+export const DownvoteButton = ({ ideaId, onSuccess, onError }) => (
+  <Button
+    {...{
+      buttonProps: {
+        className: 'image-only-downvote-button',
+      },
+      buttonContents: (
+        <img src={DownvoteButtonImg} alt='Downvote' className='downvote-icon' />
+      ),
+      fetchAddr: `/ideas/${ideaId}/downvote`,
+      ideaId,
+      onSuccess,
+      onError,
+    }}
+  />
+);
 
-export const UpvoteButton = ({ ideaId, onSuccess, onError }) => {
-  const { userState } = useUser();
-  return (
-    <Button
-      {...{
-        buttonProps: {
-          className: 'image-only-upvote-button',
-          ...(userState.upvotes.has(ideaId) && { disabled: true }),
-        },
-        buttonContents: (
-          <img src={UpvoteButtonImg} alt='Upvote' className='upvote-icon' />
-        ),
-        fetchAddr: `/ideas/${ideaId}/upvote`,
-        ideaId,
-        onSuccess,
-        onError,
-      }}
-    />
-  );
-};
+export const UpvoteButton = ({ ideaId, onSuccess, onError }) => (
+  <Button
+    {...{
+      buttonProps: {
+        className: 'image-only-upvote-button',
+      },
+      buttonContents: (
+        <img src={UpvoteButtonImg} alt='Upvote' className='upvote-icon' />
+      ),
+      fetchAddr: `/ideas/${ideaId}/upvote`,
+      ideaId,
+      onSuccess,
+      onError,
+    }}
+  />
+);
