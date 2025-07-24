d572286ffc07cdaf0c476df436c58e2b1051ec1d
Sebastian_Wlo <sebastianwlodarczyk92@gmail.com>
2025-07-24 16:32:12 +0200

    feat: added a brighter background to the profile page

--- frontend/src/pages/MePage.jsx
+++ frontend/src/pages/MePage.jsx
@@ -6,32 +6,30 @@ const MePage = () => {
   const { isLogged, userState } = useUser();
   const [loggedUser] = useState(userState);
   return (
-    <div className='section-card flex flex-col items-center justify-center min-h-[60vh]'>
+    <div className='section-card min-h-[60vh] flex'>
       {isLogged ? (
-        <>
+        <div className='card bg-base-100 p-4 w-full text-gray-600 mb-8'>
           <h1 className='section-heading'>{loggedUser.name}'s Profile</h1>
-          <div className='h-full w-full text-lg text-gray-600 mb-8 flex flex-col'>
-            <p>
-              <span className='font-bold'>Account Name:</span>{' '}
-              {loggedUser.username}
-              {loggedUser.is_admin && <span> (Admin)</span>}
-            </p>
-            <p>
-              <span className='font-bold'>Upvotes:</span>{' '}
-              {loggedUser.upvotes.size}
-            </p>
-            <p>
-              <span className='font-bold'>Downvotes:</span>{' '}
-              {loggedUser.downvotes.size}
-            </p>
-            <Link className='underline' to='/me/ideas'>
-              My ideas
-            </Link>
-            <Link className='underline' to='/me/edit'>
-              Edit Profile
-            </Link>
-          </div>
-        </>
+          <p>
+            <span className='font-bold'>Account Name:</span>{' '}
+            {loggedUser.username}
+            {loggedUser.is_admin && <span> (Admin)</span>}
+          </p>
+          <p>
+            <span className='font-bold'>Upvotes:</span>{' '}
+            {loggedUser.upvotes.size}
+          </p>
+          <p>
+            <span className='font-bold'>Downvotes:</span>{' '}
+            {loggedUser.downvotes.size}
+          </p>
+          <Link className='underline' to='/me/ideas'>
+            My ideas
+          </Link>
+          <Link className='underline' to='/me/edit'>
+            Edit Profile
+          </Link>
+        </div>
       ) : (
         <>
           <h1 className='section-heading'>No access</h1>
