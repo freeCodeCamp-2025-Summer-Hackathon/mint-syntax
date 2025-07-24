8d3d29c73e359515405c61410918cebc93e66716
Sebastian_Wlo <sebastianwlodarczyk92@gmail.com>
2025-07-25 01:45:15 +0200

    feat: added spinny and error detection to /me page
    This adds the conditional rendering logic to the 'MePage.jsx':
    1. if the user is not logged in, it shows a notification,
    2. if the 'isLoading' returns true, it shows a 'Spinny',
    3. it shows the error if data haven't loaded properly
    4. finally, if the data was received, it displays it.

--- frontend/src/pages/MePage.jsx
+++ frontend/src/pages/MePage.jsx
@@ -1,40 +1,56 @@
 import { useUser } from '../hooks/useUser';
 import { Link } from 'react-router';
+import { useEffect } from 'react';
+import { useApi } from '../hooks/useApi';
+import Spinny from '../components/Spinny';
 
 const MePage = () => {
-  const { isAdmin, isLogged, userState } = useUser();
+  const { isLoading, error, data, fetchFromApi } = useApi({
+    loadingInitially: true,
+  });
+  const { isAdmin, isLogged } = useUser();
+
+  useEffect(() => {
+    fetchFromApi(`/me`);
+  }, [fetchFromApi]);
+
   return (
     <div className='section-card min-h-[60vh] flex'>
-      {isLogged ? (
-        <div className='card bg-base-100 p-4 w-full text-gray-600 mb-8'>
-          <h1 className='section-heading'>{userState.name}'s Profile</h1>
-          <p>
-            <span className='font-bold'>Account Name:</span>{' '}
-            {userState.username}
-            {isAdmin && <span> (Admin)</span>}
-          </p>
-          <p>
-            <span className='font-bold'>Upvotes:</span> {userState.upvotes.size}
-          </p>
-          <p>
-            <span className='font-bold'>Downvotes:</span>{' '}
-            {userState.downvotes.size}
-          </p>
-          <Link className='underline' to='/me/ideas'>
-            My ideas
-          </Link>
-          <Link className='underline' to='/me/edit'>
-            Edit Profile
-          </Link>
-        </div>
-      ) : (
-        <>
-          <h1 className='section-heading'>No access</h1>
-          <p className='text-lg text-gray-600 mb-8'>
-            You have to be logged in to see this page!
-          </p>
-        </>
-      )}
+      <div className='card bg-base-100 p-4 w-full text-gray-600 mb-8'>
+        {!isLogged ? (
+          <>
+            <h1 className='section-heading'>No access</h1>
+            <p className='text-lg text-gray-600 mb-8 self-center'>
+              You have to be logged in to see this page!
+            </p>
+          </>
+        ) : isLoading ? (
+          <Spinny />
+        ) : error ? (
+          <>`${error}`</>
+        ) : (
+          <>
+            <h1 className='section-heading'>{data.name}'s Profile</h1>
+            <p>
+              <span className='font-bold'>Account Name:</span>
+              {isAdmin && <span> (Admin)</span>}
+            </p>
+            <p>
+              <span className='font-bold'>Upvotes:</span> {data.upvotes.length}
+            </p>
+            <p>
+              <span className='font-bold'>Downvotes:</span>{' '}
+              {data.downvotes.length}
+            </p>
+            <Link className='underline' to='/me/ideas'>
+              My ideas
+            </Link>
+            <Link className='underline' to='/me/edit'>
+              Edit Profile
+            </Link>
+          </>
+        )}
+      </div>
     </div>
   );
 };
