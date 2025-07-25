47896302a0b5d432f11db3467b8c6bec1c5cccf7
Božo Stojković <bozo.stojkovic@gmail.com>
2025-07-25 17:30:28 +0200

    fix: remove limit and skip from count, use UserMe model

--- backend/src/api/routes/me.py
+++ backend/src/api/routes/me.py
@@ -5,14 +5,14 @@ from fastapi import APIRouter
 from src.api.dependencies import LoggedInUser
 from src.auth import User
 from src.dependencies import Db
-from src.models import Idea, IdeaPublic, IdeasPublic, UserEditPatch
+from src.models import Idea, IdeaPublic, IdeasPublic, UserEditPatch, UserMe
 
 router = APIRouter(prefix="/me")
 
 
 @router.get("/")
 async def get_me(current_user: Annotated[User, LoggedInUser]):
-    return current_user
+    return UserMe(**current_user.model_dump())
 
 
 @router.patch("/")
@@ -33,9 +33,7 @@ async def get_ideas(
     ideas = await db.find(
         Idea, Idea.creator_id == current_user.id, limit=limit, skip=skip
     )
-    count = await db.count(
-        Idea, Idea.creator_id == current_user.id, limit=limit, skip=skip
-    )
+    count = await db.count(Idea, Idea.creator_id == current_user.id)
     return IdeasPublic(
         data=[IdeaPublic(**idea.model_dump()) for idea in ideas], count=count
     )
