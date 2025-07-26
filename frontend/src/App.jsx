c87830e0fbec4a6928f87b1393d74797c87c7cb7
Božo Stojković <bozo.stojkovic@gmail.com>
2025-07-26 20:52:09 +0200

    Update frontend/src/App.jsx
    Co-authored-by: Krzysztof G. <60067306+gikf@users.noreply.github.com>

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -63,7 +63,7 @@ function App() {
               <Route index element={<MePage />} />
               <Route path='edit' element={<MeEditPage />} />
               <Route path='ideas' element={<MyIdeasPage />}>
-                <Route path='page/:page' element={<MyIdeasPage /> } />
+                <Route path='page/:page' element={<MyIdeasPage />} />
               </Route>
             </Route>
             <Route path='logout' element={<LogoutPage />} />
