ab7115e0f95f407e4202298e884e74b12f03cfb4
Aleksandra Filipczak <206249386+Vallayah@users.noreply.github.com>
2025-07-28 00:04:54 +0200

    fix: linter

--- frontend/src/components/RegisterForm.jsx
+++ frontend/src/components/RegisterForm.jsx
@@ -49,7 +49,7 @@ const PasswordIcon = () => {
 export function RegisterForm({ redirect_to = '/' }) {
   const formRef = useRef();
   const {
-    formState: { errors, isSubmitting, isValid },
+    formState: { errors, isSubmitting },
     handleSubmit,
     register,
     getValues,
@@ -57,20 +57,28 @@ export function RegisterForm({ redirect_to = '/' }) {
 
   const { isLogged } = useUser();
   const navigate = useNavigate();
+
   const { error, response, data, fetchFromApi } = useApi({ method: 'POST' });
 
   useEffect(() => {
-    if (data && !error) navigate('/login');
-    if (error) console.log('Error:', error);
+    if (data && !error) {
+      navigate('/login');
+    }
+    if (error) {
+      console.log('Error:', error);
+    }
   }, [response, data, error, navigate]);
 
   useEffect(() => {
-    if (isLogged && redirect_to) navigate(redirect_to);
+    if (isLogged && redirect_to) {
+      navigate(redirect_to);
+    }
   }, [isLogged, navigate, redirect_to]);
 
   const onSubmit = async () => {
     try {
       await fetchFromApi('/users', {
+        method: 'POST',
         body: new FormData(formRef.current),
       });
     } catch (e) {
@@ -89,86 +97,86 @@ export function RegisterForm({ redirect_to = '/' }) {
         <label htmlFor='username' className='form-label'>
           Username: <span className='text-red-500'>*</span>
         </label>
-        <label className='input input-bordered input-sm flex items-center gap-2'>
+        <label className='input input-sm'>
           <UserIcon />
           <input
             id='username'
             {...register('username', { required: true })}
             type='text'
             placeholder='Username'
-            className='grow focus:outline-none'
+            className='input-validator'
             aria-invalid={!!errors.username}
           />
         </label>
-        {errors.username?.type === 'required' ? (
-          <p role='alert' className='text-error text-xs mt-0.5'>
-            The field "Username" is required.
-          </p>
-        ) : (
-          error &&
-          response?.status === 409 && (
-            <p role='alert' className='text-error text-xs mt-0.5'>
-              This username is already in use.
-            </p>
-          )
-        )}
       </div>
+      {errors.username?.type === 'required' ? (
+        <p role='alert' className='text-error text-xs mt-0.5'>
+          The field "Username" is required.
+        </p>
+      ) : (
+        error &&
+        response.status === 409 && (
+          <p role='alert' className='text-error text-xs mt-0.5'>
+            This username is already in use.
+          </p>
+        )
+      )}
 
       <div className='form-group'>
         <label htmlFor='name' className='form-label'>
           Name: <span className='text-red-500'>*</span>
         </label>
-        <label className='input input-bordered input-sm flex items-center gap-2'>
+        <label className='input input-sm'>
           <UserIcon />
           <input
             id='name'
             {...register('name', { required: true })}
             type='text'
             placeholder='Name'
-            className='grow focus:outline-none'
+            className='input-validator'
             aria-invalid={!!errors.name}
           />
         </label>
-        {errors.name?.type === 'required' && (
-          <p role='alert' className='text-error text-xs mt-0.5'>
-            The field "Name" is required.
-          </p>
-        )}
       </div>
+      {errors.name?.type === 'required' && (
+        <p role='alert' className='text-error text-xs mt-0.5'>
+          The field "Name" is required.
+        </p>
+      )}
 
       <div className='form-group'>
         <label htmlFor='password' className='form-label'>
           Password: <span className='text-red-500'>*</span>
         </label>
-        <label className='input input-bordered input-sm flex items-center gap-2'>
+        <label className='input input-sm'>
           <PasswordIcon />
           <input
             id='password'
             {...register('password', { required: true, minLength: 8 })}
             type='password'
             placeholder='Password'
-            className='grow focus:outline-none'
+            className='input-validator'
             aria-invalid={!!errors.password}
           />
         </label>
-        {errors.password?.type === 'required' ? (
-          <p role='alert' className='text-error text-xs mt-0.5'>
-            The field "Password" is required.
-          </p>
-        ) : (
-          errors.password?.type === 'minLength' && (
-            <p role='alert' className='text-error text-xs mt-0.5'>
-              Password needs to be at least 8 characters long.
-            </p>
-          )
-        )}
       </div>
+      {errors.password?.type === 'required' ? (
+        <p role='alert' className='text-error text-xs mt-0.5'>
+          The field "Password" is required.
+        </p>
+      ) : (
+        errors.password?.type === 'minLength' && (
+          <p role='alert' className='text-error text-xs mt-0.5'>
+            Password needs to be at least 8 characters long.
+          </p>
+        )
+      )}
 
       <div className='form-group'>
         <label htmlFor='repeatPassword' className='form-label'>
           Repeat Password: <span className='text-red-500'>*</span>
         </label>
-        <label className='input input-bordered input-sm flex items-center gap-2'>
+        <label className='input input-sm'>
           <PasswordIcon />
           <input
             id='repeatPassword'
@@ -177,25 +185,26 @@ export function RegisterForm({ redirect_to = '/' }) {
               validate: value => getValues('password') === value,
             })}
             type='password'
-            placeholder='Repeat Password'
+            placeholder='Password'
             title='Must match the password entered in the previous input field'
+            className='input-validator'
             aria-invalid={!!errors.repeatPassword}
           />
         </label>
-        {errors.repeatPassword?.type === 'required' ? (
-          <p role='alert' className='text-error text-xs mt-0.5'>
-            The field "Repeat Password" is required.
-          </p>
-        ) : (
-          errors.repeatPassword?.type === 'validate' && (
-            <p role='alert' className='text-error text-xs mt-0.5'>
-              Both passwords need to match.
-            </p>
-          )
-        )}
       </div>
+      {errors.repeatPassword?.type === 'required' ? (
+        <p role='alert' className='text-error text-xs mt-0.5'>
+          The field "Repeat Password" is required.
+        </p>
+      ) : (
+        errors.repeatPassword?.type === 'validate' && (
+          <p role='alert' className='text-error text-xs mt-0.5'>
+            Both passwords need to match.
+          </p>
+        )
+      )}
 
-      {error && response?.status !== 409 && (
+      {error && response.status !== 409 && (
         <div role='alert' className='text-error text-center text-xs mt-0.5'>
           Something went wrong, please try again later.
         </div>
