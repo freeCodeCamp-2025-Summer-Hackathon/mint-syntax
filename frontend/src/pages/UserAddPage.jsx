e5aff9eddddbefebc5e293660e1ea306e9b33c9d
Aleksandra Filipczak <206249386+Vallayah@users.noreply.github.com>
2025-07-27 12:51:29 +0200

    Update frontend/src/pages/UserAddPage.jsx
    Co-authored-by: Krzysztof G. <60067306+gikf@users.noreply.github.com>

--- frontend/src/pages/UserAddPage.jsx
+++ frontend/src/pages/UserAddPage.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router';
 import { useUser } from '../hooks/useUser';
 import { useApi } from '../hooks/useApi';
