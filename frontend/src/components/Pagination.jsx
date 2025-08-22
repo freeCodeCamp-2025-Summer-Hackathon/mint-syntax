61522929d2e0f6345fc26d76e0a1b647a7828de4
Aleksandra Filipczak <206249386+Vallayah@users.noreply.github.com>
2025-08-22 18:50:56 +0200

    Update frontend/src/components/Pagination.jsx
    Co-authored-by: Krzysztof G. <60067306+gikf@users.noreply.github.com>

--- frontend/src/components/Pagination.jsx
+++ frontend/src/components/Pagination.jsx
@@ -27,7 +27,13 @@ export const Pagination = ({
 }) => {
   const [activePage, setActivePage] = useState(initialPage);
   const pages = [...Array(numberOfPages).keys()].map(Page.fromZeroBased);
-  const { page: paramPageOneBased = '1' } = useParams();
+  const { page: paramPageOneBased = 1 } = useParams();
+  
+  window.onpopstate = () => {
+    const newPage = Page.fromOneBased(parseInt(paramPageOneBased));
+    setActivePage(newPage);
+    fetchPage(newPage);
+  }
 
   useEffect(() => {
     setActivePage(Page.fromOneBased(parseInt(paramPageOneBased)));
