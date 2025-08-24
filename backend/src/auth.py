8d7a08e78d018d20ff03c26bc9a9c18d030498c7
gikf <60067306+gikf@users.noreply.github.com>
2025-08-24 22:13:54 +0200

    feat: auto rehash password

--- backend/src/auth.py
+++ backend/src/auth.py
@@ -24,6 +24,10 @@ def verify_password(plain_password, hashed_password):
     return password_context.verify(plain_password, hashed_password)
 
 
+def verify_and_update_password(plain_password, hashed_password):
+    return password_context.verify_and_update(plain_password, hashed_password)
+
+
 def get_password_hash(plain_password):
     return password_context.hash(plain_password)
 
@@ -32,8 +36,14 @@ async def authenticate_user(db, username, plain_password) -> User | Literal[Fals
     user = await db.find_one(User, User.username == username)
     if not user:
         return False
-    if not verify_password(plain_password, user.hashed_password):
+    valid, require_rehash = verify_and_update_password(
+        plain_password, user.hashed_password
+    )
+    if not valid:
         return False
+    if require_rehash:
+        user.hashed_password = get_password_hash(plain_password)
+        await db.save(user)
     return user
 
 
