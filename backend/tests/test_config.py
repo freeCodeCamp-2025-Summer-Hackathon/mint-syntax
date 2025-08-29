a691b2abee1f8afee9bd787f90e03862cca34c3d
gikf <60067306+gikf@users.noreply.github.com>
2025-08-29 13:22:02 +0200

    test: `get_settings`

---
+++ backend/tests/test_config.py
@@ -0,0 +1,84 @@
+from contextlib import contextmanager
+from importlib import reload
+
+import pytest
+from pydantic_settings import SettingsConfigDict
+
+import src.config
+
+
+@pytest.fixture
+def env_variables():
+    return {
+        "api_location": "http://random.com:5432",
+        "csrf_secret_key": "csrf-secret-key",
+        "home_location": "http://different.random.com:8000",
+        "mongodb_uri": "mongodb://completely.different.com/name?directConnection=true",
+        "secret_key": "secret-key-for-jwt",
+    }
+
+
+@pytest.fixture
+@contextmanager
+def mock_env(monkeypatch, env_variables):
+    with monkeypatch.context() as patch:
+        for key, value in env_variables.items():
+            patch.setenv(key.upper(), value)
+        yield
+
+
+def test_get_settings_reads_from_env_file(monkeypatch, tmp_path, env_variables):
+    env_file = tmp_path / "env"
+    env_file.write_text(
+        "\n".join(f"{key.upper()}={value}" for key, value in env_variables.items())
+    )
+
+    monkeypatch.setattr(
+        src.config.Settings,
+        "model_config",
+        SettingsConfigDict(env_file=env_file, extra="ignore"),
+    )
+    config = src.config.get_settings()
+    # Make sure changed .env file is used
+    config.__init__(**{})
+
+    for key, value in env_variables.items():
+        assert getattr(config, key) == value
+
+
+def test_get_settings_reads_environment_variables(env_variables, mock_env):
+    with mock_env:
+        # Reload allows to pickup new environement variables set by mock_env
+        reload(src.config)
+        config = src.config.get_settings()
+
+        for key, value in env_variables.items():
+            assert getattr(config, key) == value
+    # Make sure module will not use patched variables anymore
+    reload(src.config)
+
+
+def test_get_settings_reads_individually_set_environment_variables(
+    monkeypatch, env_variables
+):
+    initial_config = src.config.get_settings()
+
+    for cur_key, cur_val in env_variables.items():
+        initial_value = getattr(initial_config, cur_key)
+        with monkeypatch.context() as patch:
+            patch.setenv(cur_key.upper(), cur_val)
+            reload(src.config)
+
+            config = src.config.get_settings()
+            config_value = getattr(config, cur_key)
+
+            assert initial_value != config_value
+            assert cur_val == config_value
+
+            variables_not_modified = (env for env in env_variables if env != cur_key)
+
+            assert all(
+                getattr(config, key) != env_variables.get(key)
+                and getattr(config, key) == getattr(initial_config, key)
+                for key in variables_not_modified
+            )
