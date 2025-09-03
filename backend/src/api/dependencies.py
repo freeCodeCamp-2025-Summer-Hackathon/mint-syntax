97782c4367a809873b2f3fcb3bed6b122b04be4a
Krzysztof G. <60067306+gikf@users.noreply.github.com>
2025-09-03 13:45:17 +0200

    refactor: patch dependencies (#299)

--- backend/src/api/dependencies.py
+++ backend/src/api/dependencies.py
@@ -1,6 +1,10 @@
 from fastapi import Depends
 
+from src.api.util import idea_or_404, user_or_404
 from src.auth import get_current_active_admin, get_current_active_user
 
 AdminUser = Depends(get_current_active_admin)
 LoggedInUser = Depends(get_current_active_user)
+
+IdeaFromPatchId = Depends(idea_or_404)
+UserFromPatchId = Depends(user_or_404)
