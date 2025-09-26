67670c7ad64f76d844e11c9a8964c1d7d3090301
Krzysztof G. <60067306+gikf@users.noreply.github.com>
2025-09-26 22:21:07 +0200

    chore: remove leftover mark (#328)

--- backend/tests/api/routes/test_ideas.py
+++ backend/tests/api/routes/test_ideas.py
@@ -499,7 +499,6 @@ async def test_GET_count_returns_total_number_of_ideas(
             assert data == initial_ideas + additional_ideas
 
 
-@pytest.mark.only
 @pytest.mark.integration
 @pytest.mark.anyio
 @pytest.mark.parametrize(
@@ -524,7 +523,6 @@ async def test_GET_ideas_id_returns_idea_data(user_with_client, idea_with_votes)
     }
 
 
-@pytest.mark.only
 @pytest.mark.integration
 @pytest.mark.anyio
 async def test_GET_ideas_id_returns_404_if_idea_does_not_exist(
@@ -544,7 +542,6 @@ async def test_GET_ideas_id_returns_404_if_idea_does_not_exist(
     assert "not found".casefold() in data["detail"].casefold()
 
 
-@pytest.mark.only
 @pytest.mark.integration
 @pytest.mark.anyio
 @pytest.mark.parametrize("invalid_id", ["random", "not-object-id"])
