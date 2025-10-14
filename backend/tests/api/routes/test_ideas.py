7a947be02cfe38a95624db684883e5ebb34ce96f
cosmonewt <jeyang90@gmail.com>
2025-10-14 14:20:15 +0100

    refactor: added pytest marks

--- backend/tests/api/routes/test_ideas.py
+++ backend/tests/api/routes/test_ideas.py
@@ -888,6 +888,8 @@ async def test_PATCH_ideas_id_returns_403_if_not_admin_and_not_idea_creator(
     assert response.status_code == 403
 
 
+@pytest.mark.integration
+@pytest.mark.anyio
 async def test_POST_create_idea_returns_idea(
     user_with_client,
 ):
